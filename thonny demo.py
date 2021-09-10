593b7d8e4e09a3f854df46fec3919a6d626845fb
conradye <90428686+conradye@users.noreply.github.com>
2021-09-09 20:47:48 -0400

    Thonny Demo
    demo of how to use thonny

---
+++ thonny demo.py
@@ -0,0 +1,42 @@
+print(2+6)
+print(6*2)
+print(6-2)
+print(6/2)
+print(6%2)
+print(6**2)
+print(6+10*10+6)
+print((6+10)*(10+6))
+print(7+10-(2/2))
+print((100%2)+1)
+print((2**2)+(9/3))
+print("The Square of 7 is", 7**2)
+x=10
+print(x)
+x=100.5
+x=True
+x='This is a String'
+x=[1,2,3,4,5]
+x=['H','E','L','L','O']
+print(x)
+a=5
+b=3
+c=a+b
+print(c)
+c="Hello"
+print(c[2])
+print(c[0:3])
+print(c[:3])
+print(c[2:])
+x=10.3
+print("Type of variable x is", type(x))
+a=5
+b=3.2
+c="Hello"
+print("Type of variable a is",type(a))
+print("Type of variable b is",type(b))
+print("Type of variable c is",type(c))
+
+points_obtained=15.5
+total_points=100
+percentage=((points_obtained/total_points)*100)
+print("percentage are", percentage,"%")
